c1feeb1829ed2daa35622c28c9173a8f15ac70d5
Alina Rusu <alex.alina.rusu@gmail.com>
2026-03-04 18:31:18 +0100

    add multiple data points

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import ThreePointVis from "./threePointVis/ThreePointVis";
 
+const data = new Array(1000).fill(0).map((d, id) => ({ id }));
+
 function App() {
   return (
     <div className="relative box-border h-screen p-4">
       <div className="absolute top-0 right-0 bottom-0 left-0">
-        <ThreePointVis />
+        <ThreePointVis data={data} />
       </div>
     </div>
   );
--- src/threePointVis/ThreePointVis.tsx
+++ src/threePointVis/ThreePointVis.tsx
@@ -1,17 +1,31 @@
 import { Canvas } from "@react-three/fiber";
 import Controls from "./Controls";
 
-const ThreePointVis = () => {
+interface ThreePointsVisProps {
+  data: Array<{ id: number }>;
+}
+const ThreePointVis = ({ data }: ThreePointsVisProps) => {
   return (
     // Because it's 3D all positions will take a three dimensional tuple: [x, y, z]
-    <Canvas camera={{ position: [0, 0, 5] }}>
+    <Canvas camera={{ position: [0, 0, 40] }}>
       <Controls />
       <ambientLight color="#ffffff" intensity={0.1} />
       <hemisphereLight args={["#42f5a4", "#4287f5", 1.0]} />
-      <mesh position={[0, 0, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
-        <cylinderGeometry args={[0.5, 0.5, 0.15, 32]} />
-        <meshStandardMaterial attach="material" color="#fff" />
-      </mesh>
+      {data.map((d, i) => {
+        const x = (i % 30) * 1.05;
+        const y = Math.floor(i / 30) * 1.05;
+        const z = 0;
+        return (
+          <mesh
+            key={d.id}
+            position={[x, y, z]}
+            rotation={[Math.PI * 0.5, 0, 0]}
+          >
+            <cylinderGeometry args={[0.5, 0.5, 0.15, 32]} />
+            <meshStandardMaterial attach="material" color="#fff" />
+          </mesh>
+        );
+      })}
     </Canvas>
   );
 };
